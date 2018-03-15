565f5ca78df1d8114e997c841c6545307b843b95
Garbin, Giuseppe <Giuseppe.Garbin@garmin.com>
2018-03-15 14:49:05 +0100

    Homepage created

---
+++ application/controllers/Homepage.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Homepage extends CI_Controller {
+
+	function __construct ()
+	{
+		parent::__construct();
+    //$this->output->enable_profiler(TRUE);
+  }
+
+	public function index()
+	{
+		$this->load->view('Homepage_view');
+	}
+
+}
+
+/* End of file Homepage.php */
+/* Location: ./application/controllers/Homepage.php */
---
+++ application/views/Homepage_view.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Codeigniter Cookie Law</title>
+</head>
+<body>
+
+	<h1>Hi there!</h1>
+	<h4>Welcome to the homepage</h4>
+	
+</body>
+</html>
